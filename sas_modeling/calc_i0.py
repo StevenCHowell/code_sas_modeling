b148714a4b0e560a33d55f88f78460b9ebe71fac
Steven C. Howell <steven.howell@nist.gov>
2016-12-23 12:11:36 -0500

    debugging baird's method

--- sas_modeling/calc_i0.py
+++ sas_modeling/calc_i0.py
@@ -31,19 +31,21 @@ def guinier_fit(q, iq, diq=None, dq=None, q_min=0.0, q_max=0.1, view_fit=False):
     dq2 = 2 * q[id_x] * dq[id_x]
 
     # a, b, a_err, b_err = fit_line(q2, log_iq, dlog_iq)
-    vals0 = fit_line_v0(q2, log_iq, dlog_iq)
-    vals4 = fit_line_v4(q2, log_iq, dlog_iq)
-    vals3 = fit_line_v3(q2, log_iq, dlog_iq)
-    vals2 = fit_line_v2(q2, log_iq, dlog_iq)
-    vals1 = fit_line_v1(q2, log_iq, dlog_iq)
+    # vals0 = fit_line_v0(q2, log_iq, dlog_iq)
+    # vals1 = fit_line_v1(q2, log_iq, dlog_iq)
+    # vals2 = fit_line_v2(q2, log_iq, dlog_iq)
+    # vals3 = fit_line_v3(q2, log_iq, dlog_iq)
+    # vals4 = fit_line_v4(q2, log_iq, dlog_iq)
+    # vals5 = fit_line_v5(q2, log_iq, dlog_iq)
+    vals6 = fit_line_v6(q2, log_iq, dlog_iq)
 
-    a, b, a_err, b_err = vals3; save_fname = 'fit_v3_comparison.html'
+    a, b, a_err, b_err = vals6; save_fname = 'fit_v6_comparison.html'
+    # a, b, a_err, b_err = vals5; save_fname = 'fit_v5_comparison.html'
+    # a, b, a_err, b_err = vals4; save_fname = 'fit_v4_comparison.html'
+    # a, b, a_err, b_err = vals3; save_fname = 'fit_v3_comparison.html'
     # a, b, a_err, b_err = vals2; save_fname = 'fit_v2_comparison.html'
     # a, b, a_err, b_err = vals1; save_fname = 'fit_v1_comparison.html'
-    a, b, a_err, b_err = vals0; save_fname = 'fit_v0_comparison.html'
-
-
-
+    # a, b, a_err, b_err = vals0; save_fname = 'fit_v0_comparison.html'
 
     rg = np.sqrt(-3 * a)
     rg_err = -3 / (2 * rg) * a_err
@@ -64,140 +66,13 @@ def guinier_fit(q, iq, diq=None, dq=None, q_min=0.0, q_max=0.1, view_fit=False):
     return i0, rg, i0_err, rg_err
 
 
-def fit_line_v1(x, y, dy):
-    '''
-    Fit data for y = ax + b
-    return a and b
-    '''
-
-    A = np.vstack([x / dy, 1.0 / dy]).T  # weight each row by 1 / dy
-    p, residuals, _, _ = np.linalg.lstsq(A, y / dy)
-
-    a = p[0]
-    b = p[1]
-
-    # from the docs page:
-    # residuals : {(), (1,), (K,)} ndarray
-    #   Sums of residuals; squared Euclidean 2-norm for each column in b - a*x.
-    #   If the rank of a is < N or M <= N, this is an empty array. If b is
-    #   1-dimensional, this is a (1,) shape array. Otherwise the shape is (K,).
-    # rank : int
-    #   Rank of matrix a.
-    # s : (min(M, N),) ndarray
-    #   Singular values of a.
-
-    cov = out[1]
-    a_err = np.sqrt( cov[0][0] )
-    b_err = np.sqrt( cov[1][1] ) * b
-
-    return a, b, a_err, b_err
-
-
-def fit_line_v2(x, y, dy):
-    '''
-    Fit data for y = ax + b
-    return a and b
-    essentially the same results as fit_line_v0
-    '''
-
-    out = np.polynomial.polynomial.polyfit(x, y, 1, w=1/dy, full=True)
-    # does not provide the covariance matrix, not sure how to extract error
-
-    p_final = out[0]
-    a = p_final[1]
-    b = p_final[0]
-
-    # from the docs page:
-    # [residuals, rank, singular_values, rcond] : list
-    # These values are only returned if full = True
-    #   resid – sum of squared residuals of the least squares fit
-    #   rank  – the numerical rank of the scaled Vandermonde matrix
-    #   sv    – singular values of the scaled Vandermonde matrix
-    #   rcond – value of rcond.
-    # For more details, see linalg.lstsq.
-    residuals = out[1]
-    b_err = np.sqrt( residuals[0] ) * b
-    a_err = np.sqrt( residuals[1] )
-
-    return a, b, a_err, b_err
-
-
-def fit_line_v3(x, y, dy):
-    '''
-    Fit data for y = ax + b
-    return a and b
-    method taken from SasView:
-    github.com/SasView/sasview/blob/master/src/sas/sascalc/invariant/invariant.py
-    '''
-    A = np.vstack([x / dy, 1.0 / dy]).T
-    p, residuals, _, _ = np.linalg.lstsq(A, y / dy)
-
-    a = p[0]
-    b = p[1]
-
-    # Get the covariance matrix, defined as inv_cov = a_transposed * a
-    inv_cov = np.dot(A.transpose(), A)
-    cov = np.linalg.pinv(inv_cov)
-    err_matrix = np.abs(residuals) * cov
-    a_err = np.sqrt(err_matrix[0][0])
-    b_err = np.sqrt(err_matrix[1][1])
-
-    return a, b, a_err, b_err
-
-
-def fit_line_v4(x, y, dy):
-    '''
-    Fit data for y = ax + b
-    return a and b
-    '''
-
-    p, cov = np.polyfit(x, y, 1, w=1/dy, cov=True)
-
-    a, b = p
-
-    # From docs page:
-    # The diagonal of this matrix (cov) are the
-    # variance estimates for each coefficient.
-    a_err, b_err = np.sqrt(np.diag(cov))  # standard devaitions
-
-    return a, b, a_err, b_err
-
-
-def fit_line_v5(x, y, dy):
-    '''
-    Fit data for y = ax + b
-    return a and b
-    method taken from wikipedia:
-    https://en.wikipedia.org/wiki/Linear_least_squares_(mathematics)#Python
-    '''
-
-    X = np.matrix([np.ones(len(x)), x]).T
-    Y = np.matrix(y).T
-    betaHat = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-    print(betaHat)
-    plt.figure(1)
-    xx = np.linspace(0, 5, 2)
-    yy = np.array(betaHat[0] + betaHat[1] * xx)
-
-    p, cov = np.polyfit(x, y, 1, w=1/dy, cov=True)
-
-
-
-    a, b = p
-
-    # From docs page:
-    # The diagonal of this matrix (cov) are the
-    # variance estimates for each coefficient.
-    a_err, b_err = np.sqrt(np.diag(cov))  # standard devaitions
-
-    return a, b, a_err, b_err
-
-
 def fit_line_v0(x, y, dy):
     '''
     Fit data for y = ax + b
     return a and b
     essentially the same results as fit_line_v2
+
+    resulting error seems reasonable compared to input data
     '''
 
     # define our (line) fitting function
@@ -236,9 +111,197 @@ def fit_line_v0(x, y, dy):
     #   compute the covariance matrix.
     cov = out[1]
     a_err = np.sqrt( cov[0, 0] )
-    b_err = np.sqrt( cov[1, 1] ) * b
+    b_err = np.sqrt( cov[1, 1] )
 
     return a, b, a_err, b_err
+
+
+def fit_line_v1(x, y, dy):
+    '''
+    Fit data for y = ax + b
+    return a and b
+
+    no error estimate
+    '''
+
+    A = np.vstack([x / dy, 1.0 / dy]).T  # weight each row by 1 / dy
+    p, residuals, _, _ = np.linalg.lstsq(A, y / dy)
+
+    a = p[0]
+    b = p[1]
+
+    # from the docs page:
+    # residuals : {(), (1,), (K,)} ndarray
+    #   Sums of residuals; squared Euclidean 2-norm for each column in b - a*x.
+    #   If the rank of a is < N or M <= N, this is an empty array. If b is
+    #   1-dimensional, this is a (1,) shape array. Otherwise the shape is (K,).
+    # rank : int
+    #   Rank of matrix a.
+    # s : (min(M, N),) ndarray
+    #   Singular values of a.
+
+    a_err = 0.0
+    b_err = 0.0
+
+    return a, b, a_err, b_err
+
+
+def fit_line_v2(x, y, dy):
+    '''
+    Fit data for y = ax + b
+    return a and b
+    essentially the same results as fit_line_v0
+
+    no error estimate
+    '''
+
+    out = np.polynomial.polynomial.polyfit(x, y, 1, w=1/dy, full=True)
+    # does not provide the covariance matrix, not sure how to extract error
+
+    p_final = out[0]
+    a = p_final[1]
+    b = p_final[0]
+
+    # from the docs page:
+    # [residuals, rank, singular_values, rcond] : list
+    # These values are only returned if full = True
+    #   resid – sum of squared residuals of the least squares fit
+    #   rank  – the numerical rank of the scaled Vandermonde matrix
+    #   sv    – singular values of the scaled Vandermonde matrix
+    #   rcond – value of rcond.
+    # For more details, see linalg.lstsq.
+
+    b_err = 0.0
+    a_err = 0.0
+
+    return a, b, a_err, b_err
+
+
+def fit_line_v3(x, y, dy):
+    '''
+    Fit data for y = ax + b
+    return a and b
+    method taken from SasView:
+    github.com/SasView/sasview/blob/master/src/sas/sascalc/invariant/invariant.py
+
+    error estimate seems much too small
+    '''
+    A = np.vstack([x / dy, 1.0 / dy]).T
+    p, residuals, _, _ = np.linalg.lstsq(A, y / dy)
+
+    a = p[0]
+    b = p[1]
+
+    # Get the covariance matrix, defined as inv_cov = a_transposed * a
+    inv_cov = np.dot(A.transpose(), A)
+    cov = np.linalg.pinv(inv_cov)
+    err_matrix = np.abs(residuals) * cov
+    a_err = np.sqrt(err_matrix[0][0])
+    b_err = np.sqrt(err_matrix[1][1])
+
+    return a, b, a_err, b_err
+
+
+def fit_line_v4(x, y, dy):
+    '''
+    Fit data for y = ax + b
+    return a and b
+
+    error estimate seems much too small
+    '''
+
+    p, cov = np.polyfit(x, y, 1, w=1/dy, cov=True)
+
+    a, b = p
+
+    # From docs page:
+    # The diagonal of this matrix (cov) are the
+    # variance estimates for each coefficient.
+    a_err, b_err = np.sqrt(np.diag(cov))  # standard devaitions
+
+    return a, b, a_err, b_err
+
+
+def fit_line_v5(x, y, dy):
+    '''
+    Fit data for y = ax + b
+    return a and b
+    method taken from wikipedia:
+    https://en.wikipedia.org/wiki/Linear_least_squares_(mathematics)#Python
+
+    not sure how to get M
+    '''
+
+    m = len(x)
+    X = np.array([x, np.ones(m)]).T
+    Y = np.array(y).reshape(-1, 1)
+    W = np.eye(m) * dy
+
+    # calculate the parameters
+    xtwx_inv = np.linalg.inv(X.T.dot(W).dot(X))
+    a, b = xtwx_inv.dot(X.T).dot(W).dot(Y)
+
+    # calculate the error of the parameters:
+
+    # (X.T * W * X)^-1 * X.T * W * M * W.T * X * (X.T * W.T * X)^-1
+    cov_xy = covariance(x, y)
+    var_x = covariance(x, x)
+    var_y = covariance(y, y)
+
+    xtwtx_inv = np.linalg.inv(X.T.dot(W.T).dot(X))
+    M_beta = xtwx_inv.dot(X.T).dot(W).M.dot(W.T).dot(x).dot(xtwtx_inv)
+
+    # using assumption that: M = W^-1
+    # cov = xtwx_inv
+    # a_err, b_err = np.sqrt(np.diag(cov))
+
+    return a, b, a_err, b_err
+
+
+def fit_line_v6(x, y, dy):
+    '''
+    Fit data for y = ax + b
+    return a and b
+    method taken from Experimentation by Baird: pg 138-140
+    The dy's in the derivation are not the same as the error of
+    '''
+    x = x / dy
+    y = y / dy
+
+    n = len(x)
+    sum_xy = (x * y).sum()
+    sum_x = x.sum()
+    sum_y = y.sum()
+    sum_x2 = (x ** 2).sum()
+    sum2_x = sum_x ** 2
+
+    den = n * sum_x2 - sum2_x
+
+    a_num = n * sum_xy - sum_x * sum_y
+    a = a_num / den
+
+    b_num = sum_x2 * sum_y - sum_x * sum_xy
+    b = b_num / den
+
+    y_fit = a * x + b
+
+    delta_y = y - y_fit
+
+    y_err = np.sqrt(np.sum(delta_y ** 2) / (n - 2))
+
+    a_err = y_err * np.sqrt(n / den)
+
+    b_err = y_err * np.sqrt(sum_x2 / den)
+
+    return a, b, a_err, b_err
+
+
+def covariance(x, y):
+    assert len(x) == len(y)
+
+    cov = ((x - x.mean()) * (y - y.mean())).sum() / (len(x) - 1)
+
+    return cov
 
 
 def bayesian():
@@ -249,29 +312,6 @@ if __name__ == '__main__':
     import os
     import make_figures
 
-
-    x = np.array([1, 2, 3, 4])
-    '''
-    import numpy as np
-    import matplotlib.pyplot as plt
-    input = np.array([
-        [1, 6],
-        [2, 5],
-        [3, 7],
-        [4, 10]
-    ])
-    m = np.shape(input)[0]
-    X = np.matrix([np.ones(m), input[:,0]]).T
-    y = np.matrix(input[:,1]).T
-    betaHat = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-    print(betaHat)
-    plt.figure(1)
-    xx = np.linspace(0, 5, 2)
-    yy = np.array(betaHat[0] + betaHat[1] * xx)
-    plt.plot(xx, yy.T, color='b')
-    plt.scatter(input[:,0], input[:,1], color='r')
-    plt.show()
-    '''
     data_fname = 'dev/1mgml_LysoSANS.sub'; skiprows = 1
     # data_fname = 'exp_data_lysozyme.dat'; skiprows = 0
     assert os.path.exists(data_fname)
