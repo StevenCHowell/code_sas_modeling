519af89bae217e66ed299537773e1f1dd187b368
Steven C. Howell <steven.howell@nist.gov>
2017-02-07 12:04:19 -0500

    original pure python pr-calculator

---
+++ sas_modeling/calc_pr.py
@@ -0,0 +1,92 @@
+import errno
+import logging
+import os
+import time
+
+import numpy as np
+from scipy.spatial.distance import pdist
+
+from sasmol import sasmol  # https://github.com/madscatt/sasmol
+
+
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
+def main(pdb_fname, dcd_fname='', in_dir='', out_dir=''):
+    '''
+    calculate the pair-distance distribution
+    '''
+    full_pdb_fname = os.path.join(in_dir, pdb_fname)
+    assert os.path.exists(full_pdb_fname), 'No such file: {}'.format(
+        full_pdb_fname)
+    mol = sasmol.SasMol(0)
+    mol.read_pdb(full_pdb_fname)
+
+    if dcd_fname:
+        full_dcd_fname = os.path.join(in_dir, dcd_fname)
+        assert os.path.exists(full_dcd_fname), 'No such file: {}'.format(
+            full_dcd_fname)
+        dcd_file = mol.open_dcd_read(os.path.join(in_dir, full_dcd_fname))
+        n_frames = dcd_file[2]
+        out_prefix = dcd_fname[:-4]
+
+    else:
+        n_frame = 1
+        out_prefix = pdb_fname[:-4]
+
+    if out_dir:
+        mkdir_p(out_dir)
+        out_prefix = os.path.join(out_dir, out_prefix)
+
+    tic = time.time()
+    for i in xrange(n_frames):
+        mol.read_dcd_step(dcd_file, i)
+
+        # calculated the euclidean distances
+        dist = pdist(mol.coor()[0])
+
+        # bin the distances into P(r)
+        r_max = dist.max()
+        r_min = dist.min()
+        if r_min < 0.5:
+            logging.warning('min distance less than 0.5 A: {}'.format(r_min))
+
+        n_bins = np.ceil(r_max).astype(int) + 1
+        bin_edges = np.arange(n_bins + 1) - 0.5
+        pr = np.empty([n_bins, 2])
+        pr[:, 0] = np.arange(n_bins) + 1
+        pr[:, 1], _ = np.histogram(dist, bin_edges)
+
+        # output the result
+        out_fname = '{}_{:05d}.pr'.format(out_prefix, i+1)
+        np.savetxt(out_fname, pr, fmt='%d', delimiter=',')
+
+    toc = time.time() - tic
+    logging.info('calculated P(r) for {} structures in {} s'.format(n_frames,
+                                                                    toc))
+
+    mol.close_dcd_read(dcd_file[0])
+
+
+def mkdir_p(path):
+    '''
+    make directory recursively
+    adapted from http://stackoverflow.com/questions/600268/
+    '''
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+if __name__ == '__main__':
+    pdb_fname = 'new_nl1_nrx1b_00001.pdb'
+    dcd_fname = 'new_nl1_nrx1b_1-5.dcd'
+    in_dir = '/home/schowell/data/scratch/docking'
+    out_dir = '/home/schowell/data/scratch/docking/pr'
+    main(pdb_fname, dcd_fname=dcd_fname, in_dir=in_dir)  #, out_dir=out_dir)
+
+
---
+++ sas_modeling/file_io.py
@@ -0,0 +1,15 @@
+import os
+import errno
+
+def mkdir_p(path):
+    '''
+    make directory recursively
+    adapted from http://stackoverflow.com/questions/600268/
+    '''
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
