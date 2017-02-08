9ae61f325c1e59597417ea9f540e18cec6f36f99
Steven C. Howell <steven.howell@nist.gov>
2017-02-08 15:40:00 -0500

    make library importable

---
+++ sas_modeling/__init__.py
@@ -0,0 +1,12 @@
+import sas_modeling.calc_discrepancy
+import sas_modeling.calc_i0
+import sas_modeling.compare
+import sas_modeling.file_io
+import sas_modeling.geometric
+import sas_modeling.make_figures
+
+try:
+    import sas_modeling.calc_pr
+except ModuleNotFoundError:
+    print('calc_pr not available, depends on sasmol: '
+        'https://github.com/madscatt/sasmol')
