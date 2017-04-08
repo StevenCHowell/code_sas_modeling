87ef1e1586951013c20a71eec9ca9aab9d47476c
Steven C. Howell <steven.howell@nist.gov>
2017-04-07 22:41:34 -0400

    improve python 2/3 import

--- sas_modeling/__init__.py
+++ sas_modeling/__init__.py
@@ -1,3 +1,5 @@
+#from future.utils import raise_
+
 import sas_modeling.calc_discrepancy
 import sas_modeling.calc_i0
 import sas_modeling.compare
@@ -10,17 +12,17 @@ sasmol_error = ('sasmol source: https://github.com/madscatt/sasmol')
 try:
     import sas_modeling.cluster_ensemble
 except ImportError as e:
-    print('WARNING: sas_modeling.cluster_ensemble unavailable')
-    print('ImportError: {}'.format(e.message))
-    if 'sasmol' in e.message:
+    print('\nWARNING: sas_modeling.cluster_ensemble unavailable')
+    print('ImportError: {}'.format(e))
+    if 'sasmol' in str(e):
         print(sasmol_error)
 
 try:
     import sas_modeling.calc_pr
 except ImportError as e:
-    print('WARNING: sas_modeling.calc_pr unavailable')
-    print('ImportError: {}'.format(e.message))
-    if 'sasmol' in e.message:
+    print('\nWARNING: sas_modeling.calc_pr unavailable')
+    print('ImportError: {}'.format(e))
+    if 'sasmol' in str(e):
         print(sasmol_error)
 
 del(sasmol_error)
